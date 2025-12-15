10ef8e755f8ba965804a21106558d6bedbddc720
subodhkumar86 <subodhkumar2520@gmail.com>
2025-12-15 17:23:57 +0530

    fa65

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(cors({
     'https://matrimonial-backend-7ahc.onrender.com',
     'https://matro-main-ev8s.vercel.app',
     'https://matro-main4444-bgn8.vercel.app',
+    'https://matro-main4444-4wza.vercel.app'
   ],
   credentials: true, // Only this line added
 }));
